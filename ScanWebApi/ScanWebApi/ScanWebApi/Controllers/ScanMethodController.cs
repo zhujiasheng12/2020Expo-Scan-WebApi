d1622eaa643f05a261bc1b03c7267a170de2b559
ltt102858 <821449952@qq.com>
2020-06-30 11:10:27 +0800

    alter last

--- ScanWebApi/ScanWebApi/ScanWebApi/Controllers/ScanMethodController.cs
+++ ScanWebApi/ScanWebApi/ScanWebApi/Controllers/ScanMethodController.cs
@@ -10,7 +10,7 @@ using System.Web.Http.Cors;
 
 namespace ScanWebApi.Controllers
 {
-    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
     [TokenCheckFilter]
     public class ScanMethodController : BaseApiController
     {
